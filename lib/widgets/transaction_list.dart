bcb109ca3aa5219fc2a1c33d103c144675387bdc
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-03-06 22:55:49 +0800

    Using List view instead of singleChildScrollView

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,54 +12,52 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 350,
-      child: SingleChildScrollView(
-              child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
-                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '₱ ${tx.amount.toStringAsFixed(2)}',
+                  ),
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '₱ ${tx.amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
                       style: TextStyle(
+                        fontSize: 18,
                         fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    Text(
+                      DateFormat('MMMM dd, y').format(tx.date), // Intl Package
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
                       ),
-                      Text(
-                        DateFormat('MMMM dd, y').format(tx.date), // Intl Package
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
   }
