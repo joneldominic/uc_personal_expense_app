677d571a89b6915424ef78e770767de2e19089c3
Jonel Dominic Tapang <dneljo@yahoo.com>
2020-03-06 23:59:56 +0800

    Showing modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:personal_expense_app/widgets/user_transactions.dart';
+import 'package:personal_expense_app/widgets/transaction_list.dart';
+
+import 'models/transaction.dart';
+import 'widgets/new_transaction.dart';
 
 main() => runApp(MyApp());
 
@@ -12,7 +15,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PersonalExpenseApp extends StatelessWidget {
+class PersonalExpenseApp extends StatefulWidget {
+  @override
+  _PersonalExpenseAppState createState() => _PersonalExpenseAppState();
+}
+
+class _PersonalExpenseAppState extends State<PersonalExpenseApp> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Breakfast',
+      amount: 60.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Load',
+      amount: 50.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Dinner',
+      amount: 75.00,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (bContext) {
+        return GestureDetector(
+          child: NewTransaction(_addNewTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +76,7 @@ class PersonalExpenseApp extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
           ),
         ],
       ),
@@ -38,14 +93,14 @@ class PersonalExpenseApp extends StatelessWidget {
                 child: Text('CHART'),
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
